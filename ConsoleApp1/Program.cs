a4c11dca2aca7db7f90f13ee588b69001afb606a
damian.pazvasquez@elev.ga.ntig.se <damian.pazvasquez@elev.ga.ntig.se>
2024-12-05 13:20:13 +0100

    ,ylygif

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Transactions;
+class Program
+{
+    static void Main()
+}
+    // Fråga användaren om Max-hp
+    int maxHP;
+    do{
+        Console.WriteLine("Ange karaktärens max-HP (1-20): ");
+
+        (int.TryParse(Console.ReadLine(), out max HP) && maxHP >= 1 && maxHP <= 20)
+        {
+            break;
+        }
+        else
+        {
+            
+        }
+
+        Console.WriteLine("Felaktig inmatning. Ange ett tal mellan 1 och 20.");
+    }
+    {while (true);
+
+    int currentHP = maxHP;
+
+    }
+    // Loop för att minska
+    while (currentHP > 0)
+    {
+
+    }
+    //Skriver ut
+    
+    Console.Write("[");
+    for (int i = 0; i < currentHP; i++)
+    {
+        Console.Write("=");
+    }
+    for (int i = currentHP; i < maxHP; i++)
+    {
+        Console.Write("");
+    }
+
+    Console.WriteLine("]");
+
+    //Dela ut skada
+    currentHP--;
+
+    //Vänta på att användaren trycker på en tangnent
+
+    Console.WriteLine("Tryck på valfri tangnent för att ta skada...");
+    Console.ReadKey();
+    {
+        Console.WriteLine("Karaktären har dött! Game Over.");
+    }
+
+    
